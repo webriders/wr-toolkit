fe94111e849db248354d829429bbdbf2d808a733
Olexiy Strashko <olexiy.strashko@webriders.com.ua>
2012-09-05 17:07:18 +0300

    Add setup.py for distribution

--- setup.py
+++ setup.py
@@ -1,23 +1,32 @@
 import os
-from setuptools import setup, find_packages
+try:
+    from distutils.core import setup
+except ImportError:
+    from setuptools.core import setup
+
+from wr_toolkit import __version__
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "wr_toolkit",
-    version = "0.0.1",
+    version = __version__,
     author = "WebRiders",
     author_email = "contact@webriders.com.ua",
     description = ("Kit with various tools to simplify django/python project development"),
     license = "MIT",
     keywords = "django toolkit utilities",
     url = "https://github.com/webriders/wr-toolkit",
-    packages=find_packages(),
+    packages=[
+        'wr_toolkit',
+        'wr_toolkit.webassets'
+    ],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
