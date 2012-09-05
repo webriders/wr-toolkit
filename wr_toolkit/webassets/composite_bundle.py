c99847e0fc504eeacdf56d8277bf6888febaac43
Olexiy Strashko <olexiy.strashko@webriders.com.ua>
2012-09-05 16:00:13 +0300

    Add composite bundle to simplify webassets usage

---
+++ wr_toolkit/webassets/composite_bundle.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+
+class AbstractBundle(Bundle):
+    DEFAULT_FILTERS = None
+
+    def __init__(self, *contents, **options):
+        filters = options.get('filters', None)
+        if not filters:
+            filters = self.DEFAULT_FILTERS
+        options['filters'] = filters
+        super(Bundle, self).__init__(*contents, **options)
+
+class CssBundle(AbstractBundle):
+    DEFAULT_FILTERS = settings.ASSETS_CSS_FILTERS
+
+class JsBundle(AbstractBundle):
+    DEFAULT_FILTERS = settings.ASSETS_JS_FILTERS
+
+class CompositeBundle(object):
+    def __init__(self, name, path, css=None, js=None):
+        self.name = name
+        self.css = css
+        self.js = js
+        self.path = path
+
+    @property
+    def name_css(self):
+        return self.name + '_css'
+
+    @property
+    def name_js(self):
+        return self.name + '_js'
+
+    def register(self):
+        if self.css:
+            register(self.name_css, self.css, output="%s/css/%s.css" % (self.path, self.name))
+        if self.js:
+            register(self.name_js, self.js, output="%s/js/%s.js" % (self.path, self.name))
+
+
