a834671e68dcb272bd34287ea9ccd2d99b045dd6
Andrey Sokolov <falker@gala.net>
2012-09-28 16:03:26 +0300

    Add ability to include one bundle to another

--- wr_toolkit/webassets/composite_bundle.py
+++ wr_toolkit/webassets/composite_bundle.py
@@ -34,30 +34,31 @@ class CompositeBundle(object):
     CompositeBundle(
         name = 'blog_list_all',
         path = 'blog',
+        includes = (
+            other_composite_bundle,
+        ),
         css = (
             "blog/css/blog.css",
             "blog/css/blog_list.css",
         ),
         js = (
             "blog/js/blog.js",
-            jquery_composite_bundle.js # you can include another composite bundles this way
+            jquery_composite_bundle.js
         )
     ).register()
 
     """
-    def __init__(self, name=None, path=None, css=None, js=None):
+    def __init__(self, name=None, path=None, css=None, js=None, includes=None):
+        if includes:
+            assert isinstance(includes, (tuple, list)), "'includes' parameter must be list or tuple"
+            assert all(isinstance(i, CompositeBundle) for i in includes), "All includes must be instance of CompositeBundle"
+
         self.name = name
         self.path = path
 
-        if isinstance(css, (tuple, list)):
-            self.css = CssBundle(*css)
-        else:
-            self.css = css
-
-        if isinstance(js, (tuple, list)):
-            self.js = CssBundle(*js)
-        else:
-            self.js = js
+        self.css = css or []
+        self.js = js or []
+        self.includes = includes or []
 
     @property
     def name_css(self):
@@ -67,11 +68,32 @@ class CompositeBundle(object):
     def name_js(self):
         return self.name + '_js'
 
+    def get_merged_bundles_list(self):
+        merged_css = []
+        merged_js = []
+
+        for incl in self.includes:
+            css, js = incl.get_merged_bundles_list()
+            merged_css.extend(css)
+            merged_css.extend(js)
+
+        merged_css.extend(self.css)
+        merged_css.extend(self.js)
+
+        return merged_css, merged_js
+
+    def get_merged_bundles(self):
+        css, js = self.get_merged_bundles_list()
+        return CssBundle(*css), JsBundle(*js)
+
     def register(self):
         if not self.name or not self.path:
             raise CompositeBundleError("both 'name' and 'path' must be filled")
 
-        if self.css:
-            register(self.name_css, self.css, output="%s/css/%s.css" % (self.path, self.name))
-        if self.js:
-            register(self.name_js, self.js, output="%s/js/%s.js" % (self.path, self.name))
+        merged_css, merged_js = self.get_merged_bundles()
+
+        if merged_css:
+            register(self.name_css, merged_css, output="%s/css/%s.css" % (self.path, self.name))
+
+        if merged_js:
+            register(self.name_js, merged_js, output="%s/js/%s.js" % (self.path, self.name))
